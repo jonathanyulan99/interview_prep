c3543817ef36a8052a2be7bb89c518e83e91b88c
jonathanyulan99 <77367300+jonathanyulan99@users.noreply.github.com>
2022-12-23 14:09:52 -0500

    Edits to top_view_tree.py problem -jyc

--- InterviewQuestions/Data_Structures/Tree/top_view_tree.py
+++ InterviewQuestions/Data_Structures/Tree/top_view_tree.py
@@ -9,7 +9,33 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
     
-    
+    def insert(self,val):
+        if not self.root:
+            self.root = Node(val)
+            return self.root 
+        
+        root = self.root 
+        parent = None 
+        
+        while True:
+            parent = root 
+            if val < root.value:
+                root = root.left 
+                if not root:
+                    parent.left = Node(val)
+                    return root 
+            else:
+                root =root.right 
+                if not root:
+                    parent.right = Node(val)
+                    return root 
+
+def pre_order(root):
+    if not root:
+        return 
+    print(root.value,end=' ')
+    pre_order(root.left)
+    pre_order(root.right)
 
 def top_view(root):
     if not root:
@@ -26,7 +52,7 @@ def top_view(root):
         root = queue.pop(0)
         
         if root.level not in mp:
-            mp[root.level] = root.info 
+            mp[root.level] = root.value 
         
         if root.left:
             root.left.level = root.level - 1 
@@ -36,5 +62,14 @@ def top_view(root):
             root.right.level = root.level + 1
             queue.append(root.right)
     
-    for key,value in sorted(mp):
-        print(value,end=' ')
+    for key in sorted(mp):
+        print(mp[key],end=' ')
+        
+tree = BinarySearchTree()
+
+for val in [1,2,5,3,6,4]:
+    tree.insert(val)
+
+pre_order(tree.root)
+print()
+top_view(tree.root)
